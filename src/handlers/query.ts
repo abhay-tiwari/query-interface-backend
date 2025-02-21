be1ce93aab999dcc30df3ee4d98300183501e92b
Abhay Tiwari <abhaytiwari713@gmail.com>
2025-02-21 07:10:44 +0530

    feat: added base routes

---
+++ src/handlers/query.ts
@@ -0,0 +1,9 @@
+export const getQueryResult = () => {};
+
+export const getSavedQueries = () => {};
+
+export const saveQuery = () => {};
+
+export const deleteQuery = () => {};
+
+export const updateQuery = () => {};
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
+import { deleteQuery, getQueryResult, getSavedQueries, saveQuery, updateQuery } from "./handlers/query";
 
 const router = Router();
+
+router.get("/queries", getSavedQueries);
+router.post("/queries", saveQuery);
+router.put("/queries", updateQuery);
+router.delete("/quries", deleteQuery);
+
+router.get("/query-result", getQueryResult);
 
 export default router;
